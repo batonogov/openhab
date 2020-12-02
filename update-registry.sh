4a7bf961cb433817c2e1d57a337f88ba75abeb8f
Fedor Batonogov <batonogov@outlook.com>
2020-12-02 04:45:08 +0300

    Обневлен образ

--- update-registry.sh
+++ update-registry.sh
@@ -9,6 +9,6 @@ registryserver=10.21.121.48
 registryport=32777
 host=$registryserver:$registryport
 
-docker pull openhab/$openhab
-docker tag openhab/$openhab $host/$openhab
+docker pull batonogov/openhab
+docker tag batonogov/openhab $host/$openhab
 docker push $host/$openhab
